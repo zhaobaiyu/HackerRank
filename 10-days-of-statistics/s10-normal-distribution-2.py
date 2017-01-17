d8c6aeef83dd06368c3aff6dd54fd829afd90ca1
Baiyu Zhao <git@baiyu.me>
2017-01-17 20:14:50 +0800

    170117

--- 10-days-of-statistics/s10-normal-distribution-2.py
+++ 10-days-of-statistics/s10-normal-distribution-2.py
@@ -1,11 +1,11 @@
 import math
 
-mu, delta = list(map(float, input().split()))
+mu, sigma = list(map(float, input().split()))
 x1 = float(input())
 x2 = float(input())
 
 def n(x):
-    return (1 + math.erf((x - mu) / delta / math.sqrt(2))) / 2
+    return (1 + math.erf((x - mu) / sigma / math.sqrt(2))) / 2
 
 print('{:.2f}'.format((1 - n(x1)) * 100))
 print('{:.2f}'.format((1 - n(x2)) * 100))
